a9443ea8eb6e16b18638a547e452c15c242b8f33
PiergiorgioArrigoni <piergiorgio.arrigoni@mail.polimi.it>
2020-10-11 10:01:23 +0200

    PWM code implemented

--- ARRIGONI_PIERGIORGIO.cydsn/main.c
+++ ARRIGONI_PIERGIORGIO.cydsn/main.c
@@ -7,6 +7,8 @@
 #include "project.h"
 #include "InterruptRoutine.h"
 
+#define F_CLK_PWM 12000
+
 int main(void)
 {
     uint8 config = 1; //system must start at configuration 1
@@ -19,18 +21,73 @@ int main(void)
         switch(config)
         {
             case(1):
+                Red_PWM_WritePeriod(2*F_CLK_PWM);
+                Red_PWM_SetCompareMode(1);
+                Red_PWM_WriteCompare(2*F_CLK_PWM);
+                
+                Green_PWM_WritePeriod(2*F_CLK_PWM);
+                Green_PWM_SetCompareMode(1);
+                Green_PWM_WriteCompare(2*F_CLK_PWM);
                 break;
+                
             case(2):
+                Red_PWM_WritePeriod(2*F_CLK_PWM);
+                Red_PWM_SetCompareMode(1);
+                Red_PWM_WriteCompare(2*F_CLK_PWM);
+                
+                Green_PWM_WritePeriod(2*F_CLK_PWM);
+                Green_PWM_SetCompareMode(1);
+                Green_PWM_WriteCompare(F_CLK_PWM);
                 break;
+            
             case(3):
+                Red_PWM_WritePeriod(2*F_CLK_PWM);
+                Red_PWM_SetCompareMode(3);
+                Red_PWM_WriteCompare(F_CLK_PWM);
+                
+                Green_PWM_WritePeriod(2*F_CLK_PWM);
+                Green_PWM_SetCompareMode(1);
+                Green_PWM_WriteCompare(2*F_CLK_PWM);
                 break;
+            
             case(4):
+                Red_PWM_WritePeriod(F_CLK_PWM);
+                Red_PWM_SetCompareMode(1);
+                Red_PWM_WriteCompare(0.5*F_CLK_PWM);
+                
+                Green_PWM_WritePeriod(F_CLK_PWM);
+                Green_PWM_SetCompareMode(3);
+                Green_PWM_WriteCompare(floor(0.5*F_CLK_PWM));
                 break;
+            
             case(5):
+                Red_PWM_WritePeriod(floor(0.5*F_CLK_PWM));
+                Red_PWM_SetCompareMode(3);
+                Red_PWM_WriteCompare(floor(0.25*F_CLK_PWM));
+                
+                Green_PWM_WritePeriod(floor(0.5*F_CLK_PWM));
+                Green_PWM_SetCompareMode(1);
+                Green_PWM_WriteCompare(floor(0.25*F_CLK_PWM));
                 break;
+            
             case(6):
+                Red_PWM_WritePeriod(2*F_CLK_PWM);
+                Red_PWM_SetCompareMode(1);
+                Red_PWM_WriteCompare(floor(1.5*F_CLK_PWM));
+                
+                Green_PWM_WritePeriod(2*F_CLK_PWM);
+                Green_PWM_SetCompareMode(1);
+                Green_PWM_WriteCompare(F_CLK_PWM);
                 break;
+            
             case(7):
+                Red_PWM_WritePeriod(2*F_CLK_PWM);
+                Red_PWM_SetCompareMode(3);
+                Red_PWM_WriteCompare(F_CLK_PWM);
+                
+                Green_PWM_WritePeriod(F_CLK_PWM);
+                Green_PWM_SetCompareMode(1);
+                Green_PWM_WriteCompare(floor(0.5*F_CLK_PWM));
                 break;
         }
     }
